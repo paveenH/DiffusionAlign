2f69d7997959e5dcfa109b87885688a9d296dcbc
Paveen <paveenhuang@gmail.com>
2025-06-23 16:04:38 +0800

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -1,4 +1,9 @@
 
+model_name_map = {
+    "meta-llama/Llama-3.1-8B-Instruct": "LLaMA-3.1-8B-Instruct",
+    "mistralai/Mistral-7B-Instruct-v0.3": "Mistral-7B-Instruct",
+}
+
 def get_num_blocks(model_name):
     return {
         "Llama-3.1-8B-Instruct":        32,
